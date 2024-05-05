9c5ec5823bc47e4b0e2957a914c15f2a9c1d0650
muimui69 <moises_c2015@outlook.es>
2024-05-05 16:21:29 -0400

    .env add

--- hooks/use-branch.tsx
+++ hooks/use-branch.tsx
@@ -1,12 +1,14 @@
 "use client"
+
 import { useQuery } from '@tanstack/react-query';
 import { getAllBranchs } from '@/lib/queries/branch-office';
+
 
 export function useBranchs() {
     const { data: branchs, isLoading, isError } = useQuery({
         queryKey: ['branchs'],
         queryFn: getAllBranchs
     });
-
-    return { branchs: branchs?.data?.branchs  || [], isLoading, isError };
+    
+    return { branchs: branchs?.data?.branchs || [], isLoading, isError };
 }
